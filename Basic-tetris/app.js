89418445b070b9ba2af5dc4bb9c625cfb3462f0d
Olha Vashchenko <olhavashenko@gmail.com>
2022-11-10 10:27:08 +0200

    Add moveLeft function

--- Basic-tetris/app.js
+++ Basic-tetris/app.js
@@ -87,4 +87,18 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    // move the tetromino left, unless is at the edge or there is a blocage
+    function moveLeft(){
+        undrow();
+        const isAtLeftEdge = current.some(index=>(currentPosition+index)%width===0);
+
+        if(!isAtLeftEdge) currentPosition-=1;
+
+        if(current.some(index=> squares[currentPosition+index].classList.contains("taken"))){
+            currentPosition+=1;
+        }
+
+        draw();
+    }
+
 })
